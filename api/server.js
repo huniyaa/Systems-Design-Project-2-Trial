ec5c7701c7e5fbe7c7387a4b6e6a876899769a67
Your Name <your.email@example.com>
2025-11-27 16:03:23 -0500

    vercel

--- api/server.js
+++ api/server.js
@@ -39,7 +39,7 @@ app.use(express.json())
 
 // Our API is defined in a separate module to keep things tidy.
 // Let's import our API endpoints and activate them.
-import apiRoutes from './routes/api.js'
+import apiRoutes from '../routes/api.js'
 app.use('/api', apiRoutes)
 
 // Export app for Vercel
